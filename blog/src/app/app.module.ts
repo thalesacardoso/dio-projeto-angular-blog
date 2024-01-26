a7ee01d8834be91b2415f1e41334203031c72c0d
thalesacardoso <thales.dev@hotmail.com>
2024-01-26 00:19:59 -0300

    Big card finalizado

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BigCardComponent } from './components/big-card/big-card.component';
     MenuBarComponent,
     MenuTitleComponent,
     SmallCardComponent,
-    BigCardComponent
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

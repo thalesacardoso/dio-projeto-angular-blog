ae3d50557d15037115265e7deaa2f83270bbf57d
thalesacardoso <thales.dev@hotmail.com>
2024-01-26 18:13:14 -0300

    Versão tablet + mobile parte 2

--- blog/src/app/components/big-card/big-card.component.ts
+++ blog/src/app/components/big-card/big-card.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: [
+    './big-card.component.css',
+    './big-card.component.responsivo.css'
+]
 })
 export class BigCardComponent implements OnInit {
 

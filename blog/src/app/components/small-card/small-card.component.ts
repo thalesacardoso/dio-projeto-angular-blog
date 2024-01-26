76ae60c12e7d81dd4db5faa6858f6ae684ef06ae
thalesacardoso <thales.dev@hotmail.com>
2024-01-26 17:26:52 -0300

    Versão tablet + mobile parte 1

--- blog/src/app/components/small-card/small-card.component.ts
+++ blog/src/app/components/small-card/small-card.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: [
+    './small-card.component.css',
+    './small-card.component.responsivo.css'
+]
 })
 export class SmallCardComponent implements OnInit {
 

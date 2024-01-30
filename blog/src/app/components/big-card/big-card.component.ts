fe1df036a396b743e169ad99ba5bf5c66fbc6b1f
thalesacardoso <thales.dev@hotmail.com>
2024-01-30 00:00:02 -0300

    Inserindo dados dinamicamente

--- blog/src/app/components/big-card/big-card.component.ts
+++ blog/src/app/components/big-card/big-card.component.ts
@@ -9,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 ]
 })
 export class BigCardComponent implements OnInit {
-
+  imagem: string = ""
+  title: string = ""
+  
   constructor() { }
 
   ngOnInit(): void {

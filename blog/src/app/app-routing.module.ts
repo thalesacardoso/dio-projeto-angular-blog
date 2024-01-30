6714ef78050851fc4d8175bf968f2780d64d0e25
thalesacardoso <thales.dev@hotmail.com>
2024-01-30 16:33:18 -0300

    página content criada + estilo

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',                  // vai ler o que está sendo digitado na url
+    component: HomeComponent  // Nome do componente que será mostrado na rota
+  },
+  {
+    path:'content',              
+    component: ContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ blog/src/app/pages/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  image: string = 'https://wallpapers-clan.com/wp-content/uploads/2023/11/red-ferrari-supercar-desktop-wallpaper-preview.jpg';
+  title: string = 'Aqui vai vim um titulo';
+  description: string = 'Aqui vai vim um texto grande Aqui vai vim um texto grande Aqui vai vim um texto grande Aqui vai vim um texto grande Aqui vai vim um texto grande ';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
